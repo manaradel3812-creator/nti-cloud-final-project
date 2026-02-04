f0dd7f28dd34ab65450fa79250ba33460ece9658
Ubuntu <ubuntu@ip-172-31-88-170.ec2.internal>
2026-02-04 13:23:26 +0000

    Add terraform variables files

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -21,10 +21,12 @@ resource "aws_apigatewayv2_vpc_link" "vpc_link" {
 variable "alb_arn" {
   description = "The ARN of the ALB created by Helm Ingress"
   type        = string
+  default     = null
 }
 
 resource "aws_apigatewayv2_integration" "alb_integration" {
-  api_id           = aws_apigatewayv2_api.http_api.id
+  count           = var.alb_arn != null ? 1 : 0
+  api_id          = aws_apigatewayv2_api.http_api.id
   integration_type = "HTTP_PROXY"
   integration_uri  = var.alb_arn
   connection_type  = "VPC_LINK"
@@ -35,9 +37,10 @@ resource "aws_apigatewayv2_integration" "alb_integration" {
 # Route
 # =====================================
 resource "aws_apigatewayv2_route" "default" {
+  count     = var.alb_arn != null ? 1 : 0
   api_id    = aws_apigatewayv2_api.http_api.id
   route_key = "ANY /{proxy+}"
-  target    = "integrations/${aws_apigatewayv2_integration.alb_integration.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.alb_integration[0].id}"
 }
 
 # =====================================
