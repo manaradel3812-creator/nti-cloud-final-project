eec7274c46db9d4814adca7155330f81b1ae2783
manaradel3812-creator <manaradel3812@gmail.com>
2026-02-13 06:26:16 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -77,3 +77,93 @@ variable "target_group_port" {
   type        = number
   default     = 80
 }
+# MongoDB Atlas Variables
+
+variable "mongodb_atlas_public_key" {
+  description = "MongoDB Atlas Public API Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "mongodb_atlas_private_key" {
+  description = "MongoDB Atlas Private API Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "mongodb_atlas_org_id" {
+  description = "MongoDB Atlas Organization ID"
+  type        = string
+}
+
+variable "mongodb_region" {
+  description = "MongoDB Atlas region"
+  type        = string
+  default     = "US_EAST_1"
+}
+
+variable "mongodb_instance_size" {
+  description = "MongoDB instance size"
+  type        = string
+  default     = "M10"
+}
+
+variable "mongodb_min_instance_size" {
+  description = "Minimum instance size for auto-scaling"
+  type        = string
+  default     = "M10"
+}
+
+variable "mongodb_max_instance_size" {
+  description = "Maximum instance size for auto-scaling"
+  type        = string
+  default     = "M30"
+}
+
+variable "mongodb_disk_size_gb" {
+  description = "Initial disk size in GB"
+  type        = number
+  default     = 10
+}
+
+variable "mongodb_username" {
+  description = "MongoDB database username"
+  type        = string
+  default     = "manarapp"
+}
+
+variable "mongodb_password" {
+  description = "MongoDB database password"
+  type        = string
+  sensitive   = true
+}
+
+variable "mongodb_database_name" {
+  description = "MongoDB database name"
+  type        = string
+  default     = "manarapp"
+}
+
+variable "enable_private_endpoint" {
+  description = "Enable AWS PrivateLink endpoint"
+  type        = bool
+  default     = false
+}
+
+variable "enable_vpc_peering" {
+  description = "Enable VPC Peering with MongoDB Atlas"
+  type        = bool
+  default     = false
+}
+
+variable "mongodb_atlas_cidr" {
+  description = "CIDR block for MongoDB Atlas"
+  type        = string
+  default     = "192.168.0.0/16"
+}
+
+variable "nat_gateway_ips" {
+  description = "NAT Gateway IPs to whitelist"
+  type        = list(string)
+  default     = []
+}
