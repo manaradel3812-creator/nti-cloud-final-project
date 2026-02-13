f6c35349accb9e6749b545a7b2ec3460266598ee
manaradel3812-creator <manaradel3812@gmail.com>
2026-02-13 07:54:21 +0200

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,13 +1,17 @@
 terraform {
   required_version = ">= 1.5"
-
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    mongodbatlas = {
+      source  = "mongodb/mongodbatlas"
+      version = "~> 1.15"
+    }
   }
-
+  
   backend "s3" {
     bucket  = "tfs3finalproject"
     key     = "nonprod/terraform.tfstate"
@@ -18,4 +22,9 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
+}
+
+provider "mongodbatlas" {
+  public_key  = var.mongodb_atlas_public_key
+  private_key = var.mongodb_atlas_private_key
 }
